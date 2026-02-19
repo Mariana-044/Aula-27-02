0e6ce7392222dff6231db903f81330c32442badd
MARIANA LEHR DE OLIVEIRA <mariana_l_oliveira@estudante.sesisenai.org.br>
2026-02-19 14:41:58 -0300

    Primeiro commit

--- src/components/Menu.jsx
+++ src/components/Menu.jsx
@@ -4,23 +4,17 @@ import { NavLink } from "react-router-dom";
 export default function Menu() {
   return (
     <nav className="menu">
-      <ul>
-        <li>
-          <NavLink to="/" className={({ isActive }) => (isActive ? "link active" : "link")}>
-            Home
-          </NavLink>
-        </li>
-        <li>
-          <NavLink to="/evento" className={({ isActive }) => (isActive ? "link active" : "link")}>
-            Eventos
-          </NavLink>
-        </li>
-        <li>
-          <NavLink to="/cadastrar" className={({ isActive }) => (isActive ? "link active" : "link")}>
-            Cadastrar
-          </NavLink>
-        </li>
-      </ul>
+      <NavLink to="/" className={({ isActive }) => (isActive ? "link active" : "link")}>
+        Home
+      </NavLink>
+
+      <NavLink to="/evento" className={({ isActive }) => (isActive ? "link active" : "link")}>
+        Eventos
+      </NavLink>
+
+      <NavLink to="/cadastrar" className={({ isActive }) => (isActive ? "link active" : "link")}>
+        Cadastrar
+      </NavLink>
     </nav>
   );
 }
