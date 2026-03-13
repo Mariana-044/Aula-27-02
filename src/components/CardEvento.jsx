0a981c8c1e4d4ebced8f22135f3ce45975136388
MARIANA LEHR DE OLIVEIRA <mariana_l_oliveira@estudante.sesisenai.org.br>
2026-03-13 17:22:15 -0300

    Adicionando API

--- src/components/CardEvento.jsx
+++ src/components/CardEvento.jsx
@@ -15,7 +15,7 @@ export default function CardEvento({ evento }) {
       <button onClick={() => navigate("/cadastrar", { state: evento })}>
         Editar
       </button>
-    </div>
+    </div>   
   );
 }
 
